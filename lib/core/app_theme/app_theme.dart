7574eb52fdbac1a8753e8b1fddc39f5cfa4dc269
SuvamKhadgi <appleb986@gmail.com>
2024-12-20 21:15:59 +0545

    updated app theme

---
+++ lib/core/app_theme/app_theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+      scaffoldBackgroundColor: const Color.fromARGB(255, 55, 155, 255),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Color.fromARGB(255, 0, 122, 193)),
+      fontFamily: 'Montserrat Bold',
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+        textStyle: const TextStyle(
+            fontSize: 18,
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+            fontFamily: 'Montserrat Bold'),
+        backgroundColor: Colors.blue[200],
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      )));
+}
